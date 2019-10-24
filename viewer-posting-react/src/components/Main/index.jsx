2513726fdf69a12537d8e54555afbe94e36da769
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-10-24 11:35:45 -0300

    Sub-Menu

--- viewer-posting-react/src/components/Main/index.jsx
+++ viewer-posting-react/src/components/Main/index.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import './style.css'
+import SubMenu from './SubMenu/index'
 
 export default class Main extends Component {
     render() {
         return (
             <main id='main'>
-
+                <SubMenu />
+                
             </main>
         )
     }
