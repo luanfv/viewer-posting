2337d7f2456e727bfc9bae4815e172204d725ac4
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-10-24 10:28:37 -0300

    Header

--- viewer-posting-react/src/App.js
+++ viewer-posting-react/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/index'
 
 export default () => {
   return (
     <div>
-      Hello
+      <Header />
     </div>
   )
 }
---
+++ viewer-posting-react/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import './style.css'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header id='header'>
+                <h1 className='title-header'>Viewer Posting</h1>
+            </header>
+        )
+    }
+}
