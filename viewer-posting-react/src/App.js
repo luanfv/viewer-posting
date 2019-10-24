0c23dd0aa0a08dd3fb0045defebdda23c7fcf65f
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-10-24 10:35:01 -0300

    Main

--- viewer-posting-react/src/App.js
+++ viewer-posting-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/index'
+import Main from './components/Main/index'
 
 export default () => {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   )
 }
---
+++ viewer-posting-react/src/components/Main/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import './style.css'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <main id='main'>
+
+            </main>
+        )
+    }
+}
